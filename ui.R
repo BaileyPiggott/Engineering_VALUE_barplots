b60a63c27248a79f31fca0d260930fb12509d64f
Bailey <baileypiggott@gmail.com>
2015-08-14 12:49:22 -0400

    add text to sidebar panel to describe app

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@ shinyUI(fluidPage(
   headerPanel("Eng VALUE Scores"), 
   
   sidebarPanel(
-    selectInput("discipline", "Discipline:",
+    selectInput("discipline", "Select Discipline:",
                 c("Mechanical" = 1, 
                   "Electrical and Computer" = 2, 
                   "Civil" = 4,
@@ -29,7 +29,26 @@ shinyUI(fluidPage(
                   selected = 4
                   )
                 ),
-    downloadButton("downloadPDF", "Download PDF")
+    downloadButton("downloadPDF", "Download PDF"), 
+    
+    # text description of app:
+    
+    br(),br(),
+    h4("About This App"),
+    p("This app displays second year engineering students' scores on the VALUE rubrics created 
+      by the Association of American Colleges & Universities. The work sample that was marked 
+      was the final P2 report from the APSC 200 course."),
+    p("VALUE rubrics used to assess these reports were:"),
+    tags$li("Problem Solving"),
+    tags$li("Critical Thinking"),
+    tags$li("Written Communication"),
+    br(),
+    p("Each of these three rubrics contained 5 or 6 categories. The rubrics can be found ", 
+      a("here.", href = "https://www.aacu.org/value-rubrics")),
+    br(),
+    p("Learn more about the HEQCO Learning Outcomes Assessment Project ", 
+      a("here.", href = "http://www.queensu.ca/qloa/"))
+    
 
   ),#end sidebar panel
   
